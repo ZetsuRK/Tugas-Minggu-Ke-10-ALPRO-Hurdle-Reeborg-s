ae7339921f939fac3ad6ee7bf931d40e468efbf4
VergiMutiaRahmasyani <147597045+VergiMutiaRahmasyani@users.noreply.github.com>
2023-11-17 00:36:31 +0700

    Update Tugas Minggu Ke 10 Hurdle Reeborg's.py

--- Tugas Minggu Ke 10 Hurdle Reeborg's.py
+++ Tugas Minggu Ke 10 Hurdle Reeborg's.py
@@ -25,6 +25,21 @@ for i in range(6) :
     turn_left()
 
 #Hurdle 2 :
+Hurdle 2 :
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+while not at_goal() :
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
 
 #Hurdle 3 :
 
