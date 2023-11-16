50ff0964ab70da998b3c38abca4e31c6247e8dcf
AchmadSyamsudin <147597081+AchmadSyamsudin@users.noreply.github.com>
2023-11-17 00:37:45 +0700

    Update Tugas Minggu Ke 10 Hurdle Reeborg's.py

--- Tugas Minggu Ke 10 Hurdle Reeborg's.py
+++ Tugas Minggu Ke 10 Hurdle Reeborg's.py
@@ -42,6 +42,24 @@ while not at_goal() :
     turn_left()
 
 #Hurdle 3 :
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
 
+def jump() :
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+
+while not at_goal():
+    if front_is_clear():
+       move()
+    else:
+        jump()
 
 #Hurdle 4 :
