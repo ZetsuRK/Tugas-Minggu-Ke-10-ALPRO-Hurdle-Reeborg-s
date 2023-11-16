e9d26c5fb62dcfb3696b346052ff791170357d12
VergiMutiaRahmasyani <147597045+VergiMutiaRahmasyani@users.noreply.github.com>
2023-11-17 00:28:27 +0700

    Create Tugas Minggu Ke 10 Hurdle Reeborg's.py

---
+++ Tugas Minggu Ke 10 Hurdle Reeborg's.py
@@ -0,0 +1,32 @@
+"""
+    Kelas    : 2023E
+    Prodi    : D4 Manajemen Informatika
+    Kelompok : 5
+
+    Nama Anggota :
+    1. Achmad Syamsudin       - 23091397164
+    2. Ahmad Aryobimo         - 23091397151
+    3. Vergi Mutia Rahmasyani - 23091397171
+"""
+#Hurdle 1 :
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+for i in range(6) :
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+
+#Hurdle 2 :
+
+#Hurdle 3 :
+
+
+#Hurdle 4 :
